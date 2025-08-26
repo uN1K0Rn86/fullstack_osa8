12976bfee831a28788d7ed575fafebc1c0d9ebbe
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-08-26 10:42:37 +0300

    Part 8 Exercise 2

--- library-backend/index.js
+++ library-backend/index.js
@@ -94,9 +94,16 @@ let books = [
 ]
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]
   }
 `
 
@@ -104,6 +111,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
